226c57b0b3cc4532f193fa5c0fec3caa1d62c2e0
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-12-20 20:53:03 +0800

    update

---
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+shopt -s extglob
+
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+kernel_v="$(cat include/kernel-version.mk | grep LINUX_KERNEL_HASH-4.4.* | cut -f 2 -d - | cut -f 1 -d ' ')"
+echo "KERNEL=${kernel_v}" >> $GITHUB_ENV || true
+sed -i "s?targets/%S/.*'?targets/%S/$kernel_v'?" include/feeds.mk
+
+mv -f ../feeds/ipq807x/ipq807x target/linux/
+
+rm -rf package/feeds/kiddin9/{firewall,rtl88x2bu,base-files,netifd}
+
+rm -rf devices/common/patches/{glinet,imagebuilder.patch,iptables.patch,targets.patch,kernel-defaults.patch,disable_flock.patch}
+
+
+
