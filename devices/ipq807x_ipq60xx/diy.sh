d9c3ec5d408d0fa2a8a102017dedf5f5c23a439f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-14 08:19:43 +0800

    update

--- devices/ipq807x_ipq60xx/diy.sh
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -14,10 +14,8 @@ sed -i "/wifi_ax/d" feeds.conf
 
 rm -rf package/feeds/kiddin9/{firewall,rtl*,base-files,netifd,nft-fullcone,mbedtls,oaf,shortcut-fe,simulated-driver,fast-classifier,fullconenat}
 
-rm -rf package/kernel/{nat46,ath10k-ct,mt76,rtl8812au-ct}
+rm -rf package/kernel/{ath10k-ct,mt76,rtl8812au-ct}
 rm -rf feeds/packages/net/xtables-addons package/feeds/packages/openvswitch
-
-sed -i "s/PKG_HASH:=.*/PKG_HASH:=skip/" package/feeds/wifi_ax/mac80211/Makefile
 
 rm -rf devices/common/patches/{glinet,imagebuilder.patch,iptables.patch,targets.patch,kernel-defaults.patch,disable_flock.patch}
 
