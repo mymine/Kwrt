b4af59585a2fe906af13bc1067b1c122f084032e
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-02-23 23:10:33 +0800

    update

--- devices/ipq807x_generic/diy.sh
+++ devices/ipq807x_generic/diy.sh
@@ -8,6 +8,8 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.15 target/linux/generic/hack-5.15
 svn co https://github.com/robimarko/nss-packages/trunk/qca/qca-ssdk-shell package/network/utils/qca-ssdk-shell
 
+#sh -c "curl -sfL https://github.com/robimarko/openwrt/commit/23fa931934151f72c1655ffa62ff1a979575f07e.patch | patch -d './' -p1 --forward"
+
 rm -rf feeds/kiddin9/{rtl8821cu,rtl88x2bu} package/kernel/mt76
 
 echo "
