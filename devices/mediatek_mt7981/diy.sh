4fd435f2bc978bc0e51e322fb841cb3f612ae705
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-05-22 00:59:58 +0800

    update

--- devices/mediatek_mt7981/diy.sh
+++ devices/mediatek_mt7981/diy.sh
@@ -4,8 +4,13 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf feeds/packages/utils/containerd
-svn co https://github.com/openwrt/packages/branches/openwrt-22.03/utils/containerd feeds/packages/utils/containerd
+sed -i '/postgres=5432/d' feeds/packages.index
+sed -i '/postgres=5432/d' feeds/packages/net/gnunet/Makefile
+./scripts/feeds install -a
+
+sed -i "s/make-ext4fs missing-macros/make-ext4fs meson missing-macros/" tools/Makefile
+curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/openwrt-22.03/include/meson.mk -o include/meson.mk
+svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/tools/meson tools/meson
 
 rm -rf package/feeds/kiddin9/rtl*
 
