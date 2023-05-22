e8fd4268aced4598d77b9912f200658dffe0b570
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-05-22 14:28:19 +0800

    update

--- devices/mediatek_mt7981/diy.sh
+++ devices/mediatek_mt7981/diy.sh
@@ -14,7 +14,7 @@ svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/tools/meson too
 
 rm -rf package/feeds/kiddin9/rtl*
 
-rm -rf devices/common/patches/{glinet,fix.patch,iptables.patch,kernel-defaults.patch,targets.patch}
+rm -rf devices/common/patches/{iptables.patch,kernel-defaults.patch,targets.patch}
 
 sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2024-12-06/" package/network/config/netifd/Makefile
 
