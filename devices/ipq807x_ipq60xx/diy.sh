56c9ce73b585440fcc68f45342a13d10421ebdd0
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-27 01:18:15 +0800

    update

--- devices/ipq807x_ipq60xx/diy.sh
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -9,7 +9,6 @@ mv -f ../feeds/ipq807x_v5.4/ipq60xx target/linux/ipq60xx
 
 rm -rf package/feeds
 ./scripts/feeds install -a -p ipq807x -f
-./scripts/feeds install -a -p wifi_ax -f
 ./scripts/feeds install -a -p kiddin9 -f
 ./scripts/feeds install -a
 
@@ -18,19 +17,15 @@ sed -i "/CONFIG_KERNEL_/d" .config
 #sed -i "/KernelPackage,ipt-nat6/d" package/feeds/ipq807x/linux/modules/netfilter.mk
 
 echo "
-CONFIG_FEED_gl_feeds_common=n
 CONFIG_FEED_ipq807x=n
-CONFIG_FEED_wifi_ax=n
 " >> .config
 
-sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/feeds/wifi_ax/hostapd/Makefile
+sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/feeds/ipq807x/hostapd/Makefile
 sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/network/config/netifd/Makefile
 sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/system/procd/Makefile
 sed -i "s/PKG_NAME:=iw/PKG_NAME:=iw\nPKG_SOURCE_DATE:=2099-12-06/" package/network/utils/iw/Makefile
 
-rm -rf package/feeds/ipq807x/hostapd
-
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += wireless-regdb ethtool wpad-basic-openssl/' target/linux/ipq60xx/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += wireless-regdb ethtool kmod-sched-cake wpad-basic-openssl/' target/linux/ipq60xx/Makefile
 
 #rm -rf feeds/kiddin9/{rtl*,base-files,fullconenat-nft,mbedtls,oaf,shortcut-fe,fullconenat}
 rm -rf feeds/kiddin9/base-files
