c4311d7c502992a8f2040550d98abadfd4e34de9
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-27 11:17:35 +0800

    update

--- devices/ipq807x_ipq60xx/diy.sh
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -28,10 +28,14 @@ sed -i "s/PKG_NAME:=iw/PKG_NAME:=iw\nPKG_SOURCE_DATE:=2099-12-06/" package/netwo
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += wireless-regdb ethtool kmod-sched-cake wpad-openssl/' target/linux/ipq60xx/Makefile
 
 sed -i "/KernelPackage,crypto-aead/d" package/kernel/linux/modules/crypto.mk
+sed -i "/KernelPackage,fs-xfs/d" package/kernel/linux/modules/fs.mk
+sed -i "/KernelPackage,br-netfilter/d" package/kernel/linux/modules/netfilter.mk
+sed -i "/KernelPackage,switch-ar8xxx/d" package/kernel/linux/modules/netdevices.mk
 
 
-#rm -rf feeds/kiddin9/{rtl*,base-files,fullconenat-nft,mbedtls,oaf,shortcut-fe,fullconenat}
-rm -rf feeds/kiddin9/base-files
+
+#rm -rf feeds/kiddin9/{rtl*,base-files,fullconenat-nft,mbedtls,oaf,wireguard,fullconenat}
+rm -rf feeds/kiddin9/{base-files,fullconenat-nft,oaf,rkp-ipid,shortcut-fe}
 #svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
 
 #rm -rf package/kernel/{ath10k-ct,mt76,rtl8812au-ct}
