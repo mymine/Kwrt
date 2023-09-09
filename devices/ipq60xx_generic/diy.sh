d08a86b515f07663c80e57b1e2649f1e2570480f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-09-10 01:53:00 +0800

    update

--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -2,6 +2,8 @@
 shopt -s extglob
 
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ipq60xx target/linux/ipq60xx
+svn co https://github.com/coolsnowwolf/lede/branches/package/firmware/ath11k-firmware package/firmware/ath11k-firmware
+svn co https://github.com/coolsnowwolf/lede/branches/package/firmware/ath11k-wifi package/firmware/ath11k-wifi
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-ath11k-ahb ath11k-firmware-ipq6018 wpad-basic-mbedtls/' target/linux/ipq60xx/Makefile
 make defconfig
 
