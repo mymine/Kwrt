c5fec14f921c23d638abb009e9936098238c7635
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-09-10 06:48:06 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,15 +4,8 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/feeds/packages/libpfring
-
-make defconfig
+rm -rf package/feeds/packages/libpfring package/feeds/packages/xr_usb_serial_common
 
 git clone https://github.com/JiaY-shi/nss-packages.git package/nss-packages
-
-sed -i "s/CONFIG_ALL_NONSHARED=y/CONFIG_ALL_NONSHARED=n/" .config
-sed -i "s/CONFIG_ALL_KMODS=y/CONFIG_ALL_KMODS=n/" .config
-make defconfig
-sed -i "s/# CONFIG_ALL_NONSHARED is not set/CONFIG_ALL_NONSHARED=y/" .config
 
 rm -rf feeds/kiddin9/{fibocom_QMI_WWAN,quectel_Gobinet,shortcut-fe}
