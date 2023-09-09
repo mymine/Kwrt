b433b94d91e4070f7045221dce4a6161b0e47c0d
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-09-09 21:32:36 +0800

    update

---
+++ devices/ipq60xx_generic.b/diy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+shopt -s extglob
+
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
+
+sed -i "/CONFIG_KERNEL_/d" .config
+
+rm -rf devices/common/patches/{fix.patch,targets.patch,kernel-defaults.patch,LINUX_VERSION.patch} target/imagebuilder package/network/config/netifd feeds/kiddin9/{rtl*,dnsmasq,fullconenat*,shortcut-fe} target/linux/generic/files package/feeds/luci/rpcd-mod-luci
+
+svn co https://github.com/openwrt/openwrt/branches/openwrt-21.02/target/imagebuilder target/imagebuilder
+
+sed -i "/libfakeroot/d" target/imagebuilder/Makefile
+
+svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/config/netifd package/network/config/netifd
+rm -rf package/network/config/netifd/patches
+
+sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/network/config/netifd/Makefile
+
+svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
+
+sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" feeds/kiddin9/fullconenat/Makefile
+
+sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" feeds/kiddin9/fullconenat/Makefile
+
+rm -rf toolchain/musl package/libs/libselinux package/libs/libjson-c
+
+svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/toolchain/musl toolchain/musl
+svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/libs/libselinux package/libs/libselinux
+svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/libs/libjson-c package/libs/libjson-c
+svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/utils/ucode package/utils/ucode
+#ln -sf $(pwd)/feeds/luci/modules/luci-base package/feeds/kiddin9/
--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
-
 shopt -s extglob
 
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ipq60xx target/linux/ipq60xx
+ target/linux/ipq60xx/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-ath11k-ahb ath11k-firmware-ipq6018 wpad-basic-mbedtls/' target/linux/ipq60xx/Makefile
+make defconfig
+
+svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/qca
+
+sed -i "s/+kmod-pppoe/+kmod-pppoe +kmod-bonding/" package/qca/nss/qca-nss-clients-64/Makefile
+
+rm -rf package/kernel/{qca-nss-dp,qca-ssdk}
+
+sed -i "s/CONFIG_ALL_NONSHARED=y/CONFIG_ALL_NONSHARED=n/" .config
+sed -i "s/CONFIG_ALL_KMODS=y/CONFIG_ALL_KMODS=n/" .config
+make defconfig
+sed -i "s/# CONFIG_ALL_NONSHARED is not set/CONFIG_ALL_NONSHARED=y/" .config
 
 
-sed -i "/CONFIG_KERNEL_/d" .config
-
-rm -rf devices/common/patches/{fix.patch,targets.patch,kernel-defaults.patch,LINUX_VERSION.patch} target/imagebuilder package/network/config/netifd feeds/kiddin9/{rtl*,dnsmasq,fullconenat*,shortcut-fe,luci-base} target/linux/generic/files
-
-svn co https://github.com/openwrt/openwrt/branches/openwrt-21.02/target/imagebuilder target/imagebuilder
-
-sed -i "/libfakeroot/d" target/imagebuilder/Makefile
-
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/config/netifd package/network/config/netifd
-rm -rf package/network/config/netifd/patches
-
-sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2099-12-06/" package/network/config/netifd/Makefile
-
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
-
-rm -rf toolchain/musl package/libs/libselinux package/libs/libjson-c
-
-svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/toolchain/musl toolchain/musl
-svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/libs/libselinux package/libs/libselinux
-svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/libs/libjson-c package/libs/libjson-c
-svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/utils/ucode package/utils/ucode
-#ln -sf $(pwd)/feeds/luci/modules/luci-base package/feeds/kiddin9/
