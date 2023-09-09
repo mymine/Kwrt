9eaecc75746eea54b5813630ee7afacce0e87d85
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-09-10 07:10:10 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,8 +4,6 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/feeds/packages/{libpfring,xr_usb_serial_common,coova-chilli}
-
 git clone https://github.com/JiaY-shi/nss-packages.git package/nss-packages
 
 rm -rf feeds/kiddin9/{fibocom_QMI_WWAN,quectel_Gobinet,shortcut-fe}
