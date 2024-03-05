fbe12e800caabdda516c7c40f403bed690ff4a49
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-03-06 03:41:04 +0800

    update

--- devices/rockchip_armv8/diy.sh
+++ devices/rockchip_armv8/diy.sh
@@ -5,11 +5,11 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/hack-6.1
 
-rm -rf package/feeds/kiddin9/quectel_Gobinet devices/common/patches/kernel_version.patch devices/common/patches/rootfstargz.patch target/linux/generic/hack-6.1/{410-block-fit-partition-parser.patch,724-net-phy-aquantia*,720-net-phy-add-aqr-phys.patch}
+rm -rf package/feeds/kiddin9/{quectel_Gobinet,quectel_MHI} devices/common/patches/kernel_version.patch devices/common/patches/rootfstargz.patch target/linux/generic/hack-6.1/{410-block-fit-partition-parser.patch,724-net-phy-aquantia*,720-net-phy-add-aqr-phys.patch}
 
 curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch -o target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch
 
-rm -rf package/devel/kselftests-bpf package/feeds/routing/batman-adv arm-trusted-firmware-rockchip
+rm -rf arm-trusted-firmware-rockchip
 
 rm -rf package/boot/uboot-rockchip 
 
