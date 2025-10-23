952e2a4866c3549bc98d0667ecb9d8b5a2cd68be
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-10-23 23:46:39 +0800

    update

--- devices/amlogic_mesongx/diy.sh
+++ devices/amlogic_mesongx/diy.sh
@@ -2,9 +2,11 @@
 
 shopt -s extglob
 
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/amlogic package/boot/uboot-amlogic
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/backport-6.1 target/linux/generic/hack-6.1 target/linux/generic/pending-6.1 target/linux/amlogic package/boot/uboot-amlogic
 
 sed -i "s/autocore-arm/autocore/" target/linux/amlogic/Makefile
+
+wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/include/kernel-6.1 -P include/
 
 
 
