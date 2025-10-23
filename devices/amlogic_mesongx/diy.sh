59abd01d41b595c1f22caf81d8a7bef9eb59ba4c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-10-23 15:50:06 +0800

    update

--- devices/amlogic_mesongx/diy.sh
+++ devices/amlogic_mesongx/diy.sh
@@ -2,13 +2,9 @@
 
 shopt -s extglob
 
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
-#bash $SHELL_FOLDER/../common/kernel_6.1.sh
-
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/amlogic package/boot/uboot-amlogic
 
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/mesongx.mk
+sed -i "s/autocore-arm/autocore/" target/linux/amlogic/Makefile
 
 
 
